88cd0b6d062c614ce3c03a347d2f8adb34fd4026
Daniel Lietz <ge96jek@mytum.de>
2023-08-17 19:51:26 +0200

    implemented uuid functionality in new folder structure

--- sneaker-portfolio/api/routes/portfolio.py
+++ sneaker-portfolio/api/routes/portfolio.py
@@ -6,12 +6,12 @@ router_portfolio = APIRouter()
 
 QUERY_GET_PORTFOLIO = """
     SELECT 
-        p.shoe_size, p.favorite, s.name, s.title, s.model, s.brand, s.urlKey, s.thumbUrl, s.smallImageUrl, s.imageUrl, s.description, s.retail_price, s.release_date, s.created_at
+        p.shoe_size, p.favorite, s.uuid, s.name, s.title, s.model, s.brand, s.urlKey, s.thumbUrl, s.smallImageUrl, s.imageUrl, s.description, s.retail_price, s.release_date, s.created_at
     FROM
         portfolios p
     JOIN 
         shoes s ON 
-        p.shoe_id = s.id
+        p.shoe_id = s.uuid
     WHERE 
         p.user_id = ?;
 """
@@ -33,18 +33,19 @@ async def get_user_portfolio(user_id: int):
         {
             "shoe_size": r[0],
             "favorite": r[1],
-            "name": r[2],
-            "title": r[3],
-            "model": r[4],
-            "brand": r[5],
-            "urlKey": r[6],
-            "thumbUrl": r[7],
-            "smallImageUrl": r[8],
-            "imageUrl": r[9],
-            "description": r[10],
-            "retail_price": r[11],
-            "release_date": r[12],
-            "created_at": r[13],
+            "uuid": r[2],
+            "name": r[3],
+            "title": r[4],
+            "model": r[5],
+            "brand": r[6],
+            "urlKey": r[7],
+            "thumbUrl": r[8],
+            "smallImageUrl": r[9],
+            "imageUrl": r[10],
+            "description": r[11],
+            "retail_price": r[12],
+            "release_date": r[13],
+            "created_at": r[14],
         }
         for r in results
     ]
--- sneaker-portfolio/api/routes/search.py
+++ sneaker-portfolio/api/routes/search.py
@@ -3,20 +3,21 @@ from ..db_utils import DATABASE_PATH
 from ..scrape import get_search_json
 import sqlite3
 import datetime
+import uuid
 
 router_search = APIRouter()
 
 QUERY_INSERT_SHOE = """
-    INSERT OR REPLACE 
+    INSERT OR IGNORE
     INTO shoes 
         (
-        name, title, model, brand, urlKey, thumbUrl, 
+        uuid, name, title, model, brand, urlKey, thumbUrl, 
         smallImageUrl, imageUrl, description, retail_price, 
         release_date, created_at
         )
     VALUES 
         (
-        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
         )
 """
 
@@ -27,9 +28,11 @@ def search_name(name: str):
         results = get_search_json(name)
 
         for r in results:
+            shoe_uuid = uuid.uuid5(uuid.NAMESPACE_X500, f"{r['title']}{r['urlKey']}")
             conn.cursor().execute(
                 QUERY_INSERT_SHOE,
                 (
+                    str(shoe_uuid),
                     r["name"],
                     r["title"],
                     r["model"],
